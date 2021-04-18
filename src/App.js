a28a28a5803383023ef8e5dd5b92f0b8c2d49422
Jango-070 <jango.room@hotmail.com>
2021-04-18 17:55:30 +0200

    nieuwe optie einde werkdag

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ componentDidMount(){
   render() {
     return (
       <div className="App">
-      <CardList name='jango'> 
+      <CardList name='demango'> 
         {this.state.monsters.map(monster=> (
           <h1 key={monster.id}> {monster.name} </h1>
         ))}
