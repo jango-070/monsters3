309a60df426a3894fc8d4341ea43843702f0def7
Jango-070 <jango.room@hotmail.com>
2021-04-18 12:31:15 +0200

    test met knop 24

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import { Component } from 'react';
 
 
 class App extends Component {
+  constructor(){
+    super();
+
+        this.state = {
+          string: 'hallo jangoman'
+        }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {this.state.string}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Reeeeact
-          </a>
-        </header>
+          <button onClick={() => this.setState ({string:'Hoi Rohitaaz '}) }> zie mijn echte naam</button>
+        </header>d
     </div>
     )
   }
